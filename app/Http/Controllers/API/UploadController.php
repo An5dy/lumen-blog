76ea22bf48e4176aa74c62d3b90ab2b18985b8ea
An5dy <846562014@qq.com>
2020-05-03 11:47:39 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/API/UploadController.php
+++ app/Http/Controllers/API/UploadController.php
@@ -20,14 +20,16 @@ class UploadController extends Controller
 
     public function verify()
     {
-        Log::info('server:', $_SERVER);;
+        Log::info('server:', $_SERVER);
         [$verify, $data] = Storage::verify();
 
-        if (!$verify) {
+        if (! $verify) {
             Log::warning('回调签名失败:', $data);
+
             return response()->json($data, 500);
         }
         Log::warning('回调签名:', $data);
+
         return response()->json($data, 200);
     }
 }
